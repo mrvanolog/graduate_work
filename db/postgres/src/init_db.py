5b10b5ba604c9eda7ad7e21c2836e1555a235029
mrvanolog <mrvanolog97@gmail.com>
2021-06-05 19:43:39 +0300

    add ugc init

--- db/postgres/src/init_db.py
+++ db/postgres/src/init_db.py
@@ -2,18 +2,20 @@ import os
 from pathlib import Path
 import psycopg2
 
-POSTGREWS_HOST = os.environ.get('POSTGREWS_HOST',
-                                default='rc1a-80npisto5z5x0n4n.mdb.yandexcloud.net')
-POSTGRES_PORT = os.environ.get('POSTGRES_PORT', default='6432')
-POSTGRES_USER = os.environ.get('POSTGRES_USER', default='user_admin')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', default='qwerty123')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
 path_data = Path().cwd().joinpath('data')
+
 path_film_work = path_data.joinpath('film_work.txt')
 path_genre_film_work = path_data.joinpath('genre_film_work.txt')
 path_genre = path_data.joinpath('genre.txt')
 path_person_film_work = path_data.joinpath('person_film_work.txt')
 path_person = path_data.joinpath('person.txt')
+path_users = path_data.joinpath('users.txt')
+path_ratings = path_data.joinpath('ratings.txt')
 
 path_sql_movies = Path().cwd().joinpath('init_db_movies.sql')
 with path_sql_movies.open('r') as f:
@@ -23,12 +25,16 @@ path_sql_users = Path().cwd().joinpath('init_db_users.sql')
 with path_sql_users.open('r') as f:
     sql_init_users = f.read()
 
+path_sql_users = Path().cwd().joinpath('init_db_ugc.sql')
+with path_sql_users.open('r') as f:
+    sql_init_users = f.read()
+
 
 def init_movies():
     postgres_db = 'movies'
 
     conn = psycopg2.connect(f"""
-        host={POSTGREWS_HOST}
+        host={POSTGRES_HOST}
         port={POSTGRES_PORT}
         dbname={postgres_db}
         user={POSTGRES_USER}
@@ -61,7 +67,7 @@ def init_users():
     postgres_db = 'users'
 
     conn = psycopg2.connect(f"""
-        host={POSTGREWS_HOST}
+        host={POSTGRES_HOST}
         port={POSTGRES_PORT}
         dbname={postgres_db}
         user={POSTGRES_USER}
@@ -73,15 +79,44 @@ def init_users():
     with conn.cursor() as cur:
         cur.execute(sql_init_users)
 
+        with path_users.open('r') as f:
+            cur.copy_from(f, 'auth.users')
+
     conn.commit()
     conn.close
 
     print('Таблица users успещно создана для бд users', flush=True)
 
 
+def init_ugc():
+    postgres_db = 'ugc'
+
+    conn = psycopg2.connect(f"""
+        host={POSTGRES_HOST}
+        port={POSTGRES_PORT}
+        dbname={postgres_db}
+        user={POSTGRES_USER}
+        password={POSTGRES_PASSWORD}
+        target_session_attrs=read-write
+        sslmode=verify-full
+    """)
+
+    with conn.cursor() as cur:
+        cur.execute(sql_init_users)
+
+        with path_ratings.open('r') as f:
+            cur.copy_from(f, 'user_content.ratings')
+
+    conn.commit()
+    conn.close
+
+    print('Таблица ratings успещно создана для бд ugc', flush=True)
+
+
 def main():
     init_movies()
     init_users()
+    init_ugc()
 
 
 if __name__ == '__main__':
