3510b002cda22526305aa488efa00e0d18758011
mrvanolog <mrvanolog97@gmail.com>
2021-06-09 21:05:49 +0300

    add endpoints

---
+++ rs/src/main.py
@@ -0,0 +1,30 @@
+import logging
+from fastapi import FastAPI, Depends
+
+from conf.log import set_up_logging
+from services.predictor import Predictor, get_predictor
+
+
+app = FastAPI(title='RS Predicitor API', docs_url='/rs/docs')
+
+
+@app.on_event('startup')
+async def startup_event():
+    set_up_logging()
+
+logger = logging.getLogger('main')
+
+
+@app.get('rs/healthcheck', status_code=200)
+async def health_check():
+    return "I'm alive"
+
+
+@app.post('/rs/users')
+async def predict_for_users(predictor: Predictor = Depends(get_predictor)):
+    predictor.predict_for_users()
+
+
+@app.post('/rs/movies')
+async def predict_for_movies(predictor: Predictor = Depends(get_predictor)):
+    predictor.predict_for_movies
