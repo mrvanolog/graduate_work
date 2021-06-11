fbf9185f5e43c9564c8d0ef18baeb8ce0aad053e
mrvanolog <mrvanolog97@gmail.com>
2021-06-11 17:19:10 +0300

    remove sensitive data

--- etl/src/conf/settings.py
+++ etl/src/conf/settings.py
@@ -1,17 +1,13 @@
 import os
 
 # postgres
-# POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-# POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
-# POSTGRES_USER = os.environ.get('POSTGRES_USER')
-# POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-POSTGRES_USER = 'user_admin'
-POSTGRES_PASSWORD = 'qwerty123'
-POSTGRES_HOST = 'rc1b-9g35diheb1pr3mvu.mdb.yandexcloud.net'
-POSTGRES_PORT = 6432
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
 # flush time
-FLUSH_SECONDS = int(os.environ.get('FLUSH_SECONDS', default=5))
+FLUSH_SECONDS = int(os.environ.get('FLUSH_SECONDS', default=1800))
 
 # templates
 columns_ratings = 'movie_id, user_id, rating, created_at'
