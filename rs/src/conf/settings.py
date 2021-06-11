8a8913709f16ddba4156d53a818e8c36e66f663d
mrvanolog <mrvanolog97@gmail.com>
2021-06-11 13:17:53 +0300

    add sql's for predictor

--- rs/src/conf/settings.py
+++ rs/src/conf/settings.py
@@ -11,3 +11,15 @@ sql_ratings = 'SELECT movie_id, user_id, rating FROM rs_data.ratings'
 columns_ratings = ['movie_id', 'user_id', 'rating']
 sql_movies = 'SELECT id, rating FROM rs_data.movies'
 columns_movies = ['id', 'rating']
+
+sql_delete_users = 'DELETE FROM predictions.users'
+sql_insert_users = """
+INSERT INTO predictions.users (user_id, rec_movie_id)
+VALUES %s
+"""
+
+sql_delete_movies = 'DELETE FROM predictions.movies'
+sql_insert_movies = """
+INSERT INTO predictions.movies (movie_id, rec_movie_id)
+VALUES %s
+"""
